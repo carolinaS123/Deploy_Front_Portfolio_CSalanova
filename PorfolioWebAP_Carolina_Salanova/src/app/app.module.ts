6c2eeb18d6ca74b77fdb09857f6989cce3b46045
carolinaS123 <dibunaut75@yahoo.com.ar>
2022-09-04 17:16:19 -0300

    Se creo dentro de de la carpeta componentes, el componente acerca-de, tambien el contenido del mismo

--- PorfolioWebAP_Carolina_Salanova/src/app/app.module.ts
+++ PorfolioWebAP_Carolina_Salanova/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './componentes/header/header.component';
 import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
 import { IconosSocialComponent } from './componentes/iconos-social/iconos-social.component';
 import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './componentes/banner/banner.component';
     HeaderComponent,
     LogoAPComponent,
     IconosSocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
