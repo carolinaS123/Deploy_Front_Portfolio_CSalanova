b72354a9fb6916861f7c9c091bea392d726c47fe
carolinaS123 <dibunaut75@yahoo.com.ar>
2022-09-07 13:27:26 -0300

    Se paso el contenido del componente iconos-social al de logo-ap,luego se elimino, Se creo el boton login

--- PorfolioWebAP_Carolina_Salanova/src/app/app.module.ts
+++ PorfolioWebAP_Carolina_Salanova/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
-import { IconosSocialComponent } from './componentes/iconos-social/iconos-social.component';
 import { BannerComponent } from './componentes/banner/banner.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { EducacionComponent } from './componentes/educacion/educacion.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './componentes/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    IconosSocialComponent,
     BannerComponent,
     AcercaDeComponent,
     EducacionComponent,
